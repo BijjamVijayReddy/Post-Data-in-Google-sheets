bb35bdbfdd2dac0b842550336634bec9691a1178
Bijjamvijayreddy <bijjamvijayreddy@gmail.com>
2023-10-31 15:53:06 +0530

    Google Sheets are Working Fine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+ *  1.Create the New Google Sheet and Copy the URL and Copy in the NotePad.. 
+ *  2. Open the Extension in Google Sheet and Select the Apps Script..
+ *  3. copy the Below Code:  const sheets = SpreadsheetApp.openByUrl("https://docs.google.com/spreadsheets/d/10AuRzRugSjs8nz6jT1LWzpNURh1pnvBcxZUguhfaT24/edit#gid=0");
+     //if you have changed your sheet name then replace the below Sheet1 with your sheet name
+     const sheet = sheets.getSheetByName("Sheet1");
+    function doPost(e){
+    let data = e.parameter;
+    sheet.appendRow([data.UserName,data.Userage,data.Usersalary,data.Userhobby]);
+    return ContentService.createTextOutput("Your message was successfully sent to the Googlesheet database!");
+     }
+  * Changes according the Above Code in sheet.appenRow.
+  * Deploy to New Deployment and select type to web app and  Changes to anyone in "Who can Access" and Click the Deploy
+  * Give Authcation and choose Advance and allow that it's ...
 
-function App() {
+ */
+
+import "./App.css";
+
+export default function App() {
+  function Submit(e) {
+    e.preventDefault()
+    const formEle = document.querySelector("form");
+    const formDatab = new FormData(formEle);
+    fetch(
+      'https://script.google.com/macros/s/AKfycbweqWTT4_HcxKfSopxnSiU6MizrrvMeapxeaXZPbHo2edkYpMXLmo8FJejv4RX-Gs9p/exec',
+      {
+        method: "POST",
+        body: formDatab
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Contact Form </h1>
+      <div>
+        <form className="form" onSubmit={(e) => Submit(e)}>
+          <input placeholder="Your Name" name="Name" type="text" />
+          <input placeholder="Your Email" name="Email" type="text" />
+          <input placeholder="Your Message" name="Message" type="text" />
+          <input name="Name" type="submit" />
+        </form>
+      </div>
     </div>
   );
 }
 
-export default App;
+
